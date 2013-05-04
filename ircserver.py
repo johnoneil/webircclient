363b38e702a677a4810b79709d5b0cd918b08b58
johnoneil <oneil.john@gmail.com>
2013-05-04 00:21:57 -0700

    Added simple command passing from client up to IRC server. Appears to work.

--- ircserver.py
+++ ircserver.py
@@ -14,25 +14,47 @@ import tornado.websocket
 import tornado.ioloop
 import tornado.iostream
 import socket
+import string
 
+class IRCClient:
+  def ProcessServerMessage(self, msg):
+    #the IRC message (from server) will begin with either a user ID or
+    #a command. If the first space delimited word in the message begins with ':'
+    #then it's a user ID, and command is second. If not, it's a command
+    (prefix, payload) = string.split(msg,maxsplit=1)
+    if( prefix.startswith(":") ):
+      pass
+    else:
+      if(prefix.upper() == 'PING'):
+        print 'Processing PING msg for ' + payload
+        return 'PONG ' + payload
+    return None
+
+  def ProcessClientCommand(self, msg):
+    if msg.startswith('/'):
+      return msg[1:].strip() + '\r\n'
+    else:
+      #TODO: send message to current channel
+      pass
 
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render("main.html")
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
-  #def initialize(self, stream):
-    #print 'initialize called'
+  def initialize(self):
+    print 'initialize called'
+    self.IRCClient = IRCClient()
 
   def on_connect(self):
-    nick = 'nick'
-    user = 'user'
-    pw = 'pw'
-    self.stream.write('NICK '+ nick + '\r\nUSER ' + user + '\r\nPASS ' + pw +'\r\n')
     self.stream.read_until('\r\n', self.on_line)
 
   def on_line(self, data):
+    print data
     self.write_message(u"MSG: " + data)
+    response = self.IRCClient.ProcessServerMessage(data)
+    if response is not None:
+      self.stream.write(response)
     self.stream.read_until('\r\n', self.on_line)
 
   def on_stream_close():
@@ -45,7 +67,11 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
     self.stream.connect(("192.168.1.6", 6660), self.on_connect)
 
   def on_message(self, message):
-    self.write_message(u"Echo: " + message)
+    cmd = self.IRCClient.ProcessClientCommand(message)
+    if cmd is not None:
+      print 'sending command ' + cmd + ' to host'
+      self.stream.write(str(cmd))
+
   def on_close(self):
     print 'websocket close'
  
